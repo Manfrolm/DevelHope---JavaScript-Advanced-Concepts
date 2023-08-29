c97dc5a76778a4c01577bac78bfbe2f476e10e53
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-08-29 13:05:11 +0200

    Update exercise.js

--- JS 19 - Getters and Setters/exercise.js
+++ JS 19 - Getters and Setters/exercise.js
@@ -1,29 +1,41 @@
-class Person{
-  constructor(firstName, lastName, age){
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  //Getter e Setter firstName
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(newFirstName) {
+    this._firstName = newFirstName;
+  }
+
+  //Getter e Setter lastName
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  set lastName(newLastName) {
+    this._lastName = newLastName;
+  }
+
+
+  //Getter fullName
+  get fullName(){
+    return `${this.firstName} ${this.lastName}`
+  }
+
 }
 
-set first(value){
-	this.firstName = value;
-}
-
-
-set last(value){
-	this.lastName = value;
-}
-
-
-get fullName(){
-	return this.firstName + " " + this.lastName; 
-}
-
-}
-
-const person = new Person('Mario', 'Rossi', 25);
+const person = new Person("Mario", "Rossi", 25);
 console.log(person.fullName);
 
-person.firstName = 'Maria';
-person.lastName = 'Verdi';
+person.firstName = "Maria";
+person.lastName = "Verdi";
 console.log(person.fullName);
