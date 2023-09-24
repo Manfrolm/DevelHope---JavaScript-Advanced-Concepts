6ef7a0eab80fed59e97f72327c8f73432b025ccd
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-09-24 18:21:33 +0200

    Update exercise.js

--- JS 34 - Callbacks/exercise.js
+++ JS 34 - Callbacks/exercise.js
@@ -1,8 +1,17 @@
 function printAsyncName(callback, name) {
-    let interval = setInterval(() => console.log("Hello"), 1000)
-    setTimeout(() => clearInterval(interval), 1000);
+  let interval = setInterval(() => {
+    callback();
+  }, 1000);
 
-    setTimeout(() => console.log(name), 2000);
+  setTimeout(() => {
+    clearInterval(interval);
+  }, 1000);
+
+  setTimeout(() => {
+    console.log(name);
+  }, 2000);
 }
 
-printAsyncName(() => {}, "Manfredi");
+printAsyncName(() => {
+  console.log("Hello");
+}, "Manfredi");
